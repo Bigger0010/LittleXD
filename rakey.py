23d18efc8afe746e7ca569e8e51f16bfd5538d08
Bigger00 <43074670+Bigger0010@users.noreply.github.com>
2018-09-10 03:01:50 +0700

    Update rakey.py

--- rakey.py
+++ rakey.py
@@ -7,7 +7,7 @@ from time import sleep
 from bs4 import BeautifulSoup
 from humanfriendly import format_timespan, format_size, format_number, format_length
 import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib
-parsef
+parse
 from gtts import gTTS
 from googletrans import Translator
 #==============================================================================#
